dbd1a3ba1425bfdee5e61fdc69a40177a8f25fd4
Bruno Hass <bruno_hass@live.com>
2018-10-29 19:49:35 -0300

    Implement pi table computing

--- src/matcher.rs
+++ src/matcher.rs
@@ -1,17 +1,27 @@
 fn compute_pi_table(pattern: &String) -> Vec<u32> {
-    let mut pi_table = Vec::new();
-    let pattern_arr = pattern.chars();
+    let mut pi_table: Vec<u32> = Vec::new();
+    let pattern_arr = pattern.as_bytes();
    
     pi_table.push(0);
     
-    let mut j = 0;
-    for c in pattern_arr[1..] {
+    let mut j: usize = 0;
+    for &c in pattern_arr[1..].iter() {
+        
         if c == pattern_arr[j] {
             j += 1;
-            pi_table.push(j);
+            pi_table.push(j as u32);
         } else {
-            j = pi_table.get(j - 1).unwrap_or(0);
-            pi_table.push(0);
+            if j == 0 {
+                j = pi_table[0] as usize;
+            } else {
+                j = pi_table[j - 1] as usize;
+            }
+            
+            if c == pattern_arr[j] {
+                j += 1;
+            }
+            pi_table.push(j as u32);
+            
         }
     }
 
