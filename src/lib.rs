18641c7c3bced8515e540f35bfff704723589d7c
Bruno Hass <bruno_hass@live.com>
2018-11-03 13:56:42 -0300

    Refactors KMP search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::error::Error;
 
 #[cfg(test)]
 mod tests {
@@ -27,10 +28,31 @@ And a Heaven in a Wild Flower.
 Hold Infinity in the palm of your hand. And Eternity in an hour. ";
         assert_eq!(
             vec!("And a Heaven in a Wild Flower."),
-            kmp("Heaven", text)
+            search_kmp("Heaven", text)
         );
     }
 
+}
+
+pub struct Config {
+    pattern: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+        
+        let pattern = args[1].clone();
+        
+        Ok(Config { pattern })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    
+    Ok(())
 }
 
 fn compute_pi_table(pattern: &str) -> Vec<usize> {
@@ -62,28 +84,35 @@ fn compute_pi_table(pattern: &str) -> Vec<usize> {
     return pi_table;
 }
 
-pub fn kmp<'a>(pattern: &str, text: &'a str) -> Vec<&'a str> {
-    let pi_table = compute_pi_table(pattern);
+fn kmp<'a>(pattern: &str, pi_table: &Vec<usize>, text: &'a str) -> bool {
     let mut pi_table_index: usize = 0;
     let patterns_bytes = (*pattern).as_bytes();
-
-    let mut result = Vec::new();
     
-    for line in text.lines() {
-        for &c in line.as_bytes() {
-            if c == patterns_bytes[pi_table_index] as u8 {
-                pi_table_index += 1;
-                if pi_table_index >= pi_table.len() - 1 {
-                    result.push(line);
-                    break;
-                }
-            } else {
-                if pi_table_index == 0 {
-                    pi_table_index = pi_table[0];
-                } else {
-                    pi_table_index = pi_table[pi_table_index - 1];
-                }
+    for &c in text.as_bytes() {
+        if c == patterns_bytes[pi_table_index] as u8 {
+            pi_table_index += 1;
+            if pi_table_index >= pi_table.len() - 1 {
+                return true;
             }
+        } else {
+            if pi_table_index == 0 {
+                pi_table_index = pi_table[0];
+            } else {
+                pi_table_index = pi_table[pi_table_index - 1];
+            }
+        }
+    }
+    
+    false
+}
+
+pub fn search_kmp<'a>(pattern: &str, text: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    let pi_table = compute_pi_table(pattern);
+
+    for line in text.lines() {
+        if kmp(pattern, &pi_table, line) {
+            result.push(line);
         }
     }
 
